80eead266532d29ec944f7cad77fc1a988a95cc9
Orlando Smits <orlandosmits@gmail.com>
2022-07-27 13:08:59 +0200

    Added scrollable appbar in Article view

--- lib/presentation/screens/articles/article_screen.dart
+++ lib/presentation/screens/articles/article_screen.dart
@@ -19,198 +19,210 @@ class ArticleScreen extends StatelessWidget {
         if (state.status == FetchArticleStatus.success) {
           var article = state.article;
           return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.white,
-              iconTheme: const IconThemeData(color: Color(0xff009EE0)),
-              elevation: 0.0,
-            ),
-            body: ListView(
-              children: [
-                Stack(
-                  alignment: Alignment.bottomCenter,
+            body: NestedScrollView(
+              headerSliverBuilder: (ctx, innerBoxIsScrolled) {
+                return [
+                  const SliverAppBar(
+                    backgroundColor: Colors.white,
+                    iconTheme: IconThemeData(color: Color(0xff009EE0)),
+                    elevation: 0.0,
+                    floating: false,
+                    expandedHeight: 56,
+                  )
+                ];
+              },
+              body: MediaQuery.removePadding(
+                context: context,
+                removeTop: true,
+                child: ListView(
                   children: [
                     Image.network(article.image),
-                  ],
-                ),
-                Container(
-                  color: Colors.black87,
-                  child: Stack(
-                    alignment: Alignment.topCenter,
-                    children: [
-                      Transform(
-                        transform: Matrix4.skewY(-0.06),
-                        child: Container(
-                            height: 200,
-                            width: MediaQuery.of(context).size.width,
-                            color: Colors.black,
-                            child: Column(
-                              children: [],
-                            )),
-                      ),
-                      Column(
+                    Container(
+                      color: Colors.black87,
+                      child: Stack(
+                        alignment: Alignment.topCenter,
                         children: [
-                          Container(
-                            padding: const EdgeInsets.all(2.0),
-                            decoration: BoxDecoration(
-                                border: Border.all(width: 2),
+                          Transform(
+                            transform: Matrix4.skewY(-0.06),
+                            child: Container(
+                                height: 200,
+                                width: MediaQuery.of(context).size.width,
                                 color: Colors.black,
-                                borderRadius: const BorderRadius.only(
-                                  bottomLeft: Radius.circular(10.0),
+                                child: Column(
+                                  children: const [],
                                 )),
-                            child: const Text(
-                              'PRO',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 12.0),
-                            ),
                           ),
-                          Container(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Text(
-                              article.title,
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 22.0),
-                            ),
-                          ),
-                          Container(
-                            padding:
-                                const EdgeInsets.only(left: 16.0, top: 8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                const Icon(
-                                  Icons.access_time_outlined,
-                                  color: Colors.grey,
-                                  size: 15.0,
+                          Column(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(2.0),
+                                decoration: BoxDecoration(
+                                    border: Border.all(width: 2),
+                                    color: Colors.black,
+                                    borderRadius: const BorderRadius.only(
+                                      bottomLeft: Radius.circular(10.0),
+                                    )),
+                                child: const Text(
+                                  'PRO',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12.0),
                                 ),
-                                const Spacer(),
-                                Text(
-                                  article.time,
-                                  style: const TextStyle(color: Colors.white),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  article.title,
+                                  style: const TextStyle(
+                                      color: Colors.white, fontSize: 22.0),
                                 ),
-                                const Spacer(),
-                                Stack(
-                                  alignment: Alignment.center,
+                              ),
+                              Container(
+                                padding:
+                                    const EdgeInsets.only(left: 16.0, top: 8.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
                                   children: [
                                     const Icon(
-                                      Icons.mode_comment_outlined,
-                                      semanticLabel: 'Hello?',
-                                      color: Colors.white,
-                                      size: 25,
+                                      Icons.access_time_outlined,
+                                      color: Colors.grey,
+                                      size: 15.0,
                                     ),
-                                    Container(
-                                      padding: const EdgeInsets.only(bottom: 4),
-                                      child: Text(
-                                        article.amountOfComments.toString(),
-                                        style: const TextStyle(
-                                            color: Colors.white, fontSize: 10),
-                                      ),
+                                    const Spacer(),
+                                    Text(
+                                      article.time,
+                                      style:
+                                          const TextStyle(color: Colors.white),
                                     ),
+                                    const Spacer(),
+                                    Stack(
+                                      alignment: Alignment.center,
+                                      children: [
+                                        const Icon(
+                                          Icons.mode_comment_outlined,
+                                          semanticLabel: 'Hello?',
+                                          color: Colors.white,
+                                          size: 25,
+                                        ),
+                                        Container(
+                                          padding:
+                                              const EdgeInsets.only(bottom: 4),
+                                          child: Text(
+                                            article.amountOfComments.toString(),
+                                            style: const TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 10),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const Spacer(flex: 30),
                                   ],
                                 ),
-                                const Spacer(flex: 30),
-                              ],
-                            ),
+                              ),
+                              Container(
+                                padding:
+                                    const EdgeInsets.only(left: 16.0, top: 8.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      padding: const EdgeInsets.only(
+                                          left: 8, right: 8),
+                                      decoration: BoxDecoration(
+                                        color: Colors.black,
+                                        border: Border.all(color: Colors.white),
+                                        borderRadius: const BorderRadius.all(
+                                            Radius.circular(20)),
+                                      ),
+                                      child: Row(
+                                        children: const [
+                                          Icon(
+                                            Icons.bookmark_border_outlined,
+                                            color: Colors.white,
+                                            size: 15,
+                                          ),
+                                          Text(
+                                            'Opslaan',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    const Spacer(),
+                                    Container(
+                                      padding: const EdgeInsets.all(2),
+                                      decoration: BoxDecoration(
+                                          color: Colors.grey.shade600,
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(20))),
+                                      child: const Icon(
+                                        Icons.facebook_rounded,
+                                        color: Colors.blueAccent,
+                                        size: 20,
+                                      ),
+                                    ),
+                                    const Spacer(),
+                                    Container(
+                                      padding: const EdgeInsets.all(2),
+                                      decoration: BoxDecoration(
+                                          color: Colors.grey.shade600,
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(20))),
+                                      child: const Icon(
+                                        Icons.ac_unit,
+                                        color: Colors.blue,
+                                        size: 20,
+                                      ),
+                                    ),
+                                    const Spacer(),
+                                    Container(
+                                      padding: const EdgeInsets.all(2),
+                                      decoration: BoxDecoration(
+                                          color: Colors.grey.shade600,
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(20))),
+                                      child: const Icon(
+                                        Icons.whatsapp_rounded,
+                                        color: Colors.green,
+                                        size: 20,
+                                      ),
+                                    ),
+                                    const Spacer(flex: 30),
+                                  ],
+                                ),
+                              )
+                            ],
                           ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(left: 16, top: 16),
+                      child: Row(
+                        children: [
+                          const Text(
+                            "Door ",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text(article.author.name),
                           Container(
-                            padding:
-                                const EdgeInsets.only(left: 16.0, top: 8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Container(
-                                  padding:
-                                      const EdgeInsets.only(left: 8, right: 8),
-                                  decoration: BoxDecoration(
-                                    color: Colors.black,
-                                    border: Border.all(color: Colors.white),
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(20)),
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.bookmark_border_outlined,
-                                        color: Colors.white,
-                                        size: 15,
-                                      ),
-                                      const Text(
-                                        'Opslaan',
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                const Spacer(),
-                                Container(
-                                  padding: const EdgeInsets.all(2),
-                                  decoration: BoxDecoration(
-                                      color: Colors.grey.shade600,
-                                      borderRadius: const BorderRadius.all(
-                                          Radius.circular(20))),
-                                  child: const Icon(
-                                    Icons.facebook_rounded,
-                                    color: Colors.blueAccent,
-                                    size: 20,
-                                  ),
-                                ),
-                                const Spacer(),
-                                Container(
-                                  padding: const EdgeInsets.all(2),
-                                  decoration: BoxDecoration(
-                                      color: Colors.grey.shade600,
-                                      borderRadius: const BorderRadius.all(
-                                          Radius.circular(20))),
-                                  child: const Icon(
-                                    Icons.ac_unit,
-                                    color: Colors.blue,
-                                    size: 20,
-                                  ),
-                                ),
-                                const Spacer(),
-                                Container(
-                                  padding: const EdgeInsets.all(2),
-                                  decoration: BoxDecoration(
-                                      color: Colors.grey.shade600,
-                                      borderRadius: const BorderRadius.all(
-                                          Radius.circular(20))),
-                                  child: const Icon(
-                                    Icons.whatsapp_rounded,
-                                    color: Colors.green,
-                                    size: 20,
-                                  ),
-                                ),
-                                const Spacer(flex: 30),
-                              ],
+                            margin: const EdgeInsets.only(left: 4),
+                            child: CircleAvatar(
+                              radius: 20.0,
+                              backgroundImage:
+                                  NetworkImage(article.author.image),
                             ),
                           )
                         ],
                       ),
-                    ],
-                  ),
+                    ),
+                    const Text('Heel veel text hier straks'),
+                  ],
                 ),
-                Container(
-                  padding: const EdgeInsets.only(left: 16, top: 16),
-                  child: Row(
-                    children: [
-                      const Text(
-                        "Door ",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(article.author.name),
-                      Container(
-                        margin: const EdgeInsets.only(left: 4),
-                        child: CircleAvatar(
-                          radius: 20.0,
-                          backgroundImage: NetworkImage(article.author.image),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Text('Heel veel text hier straks'),
-              ],
+              ),
             ),
           );
         } else {
